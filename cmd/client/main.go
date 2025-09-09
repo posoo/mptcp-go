c36e6ef61d043f0d4c0c51b2cdb7119649c8536e
Sen Lin <sen.lin@u.northwestern.edu>
2025-09-09 18:54:50 -0500

    feat: --no-mptcp flag for client

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net"
@@ -10,22 +11,30 @@ import (
 )
 
 func main() {
+	noMptcp := flag.Bool("no-mptcp", false, "Use regular TCP instead of MPTCP")
+	flag.Parse()
+	args := flag.Args()
 	// Get remote address and message count from command line
-	if len(os.Args) != 3 {
-		fmt.Println("Usage: client <remote_address> <message_count>")
+	if len(args) != 2 {
+		fmt.Println("Usage: client [--no-mptcp] <remote_address> <message_count>")
 		os.Exit(1)
 	}
-	remoteAddr := os.Args[1]
-	messageCount, err := strconv.Atoi(os.Args[2])
+	fmt.Println(args)
+	remoteAddr := args[0]
+	messageCount, err := strconv.Atoi(args[1])
 	if err != nil {
 		fmt.Println("Failed to parse message count:", err)
 		os.Exit(1)
 	}
 
 	// Initiate MPTCP connection
-	log.Println("Creating single MPTCP connection...")
 	dialer := &net.Dialer{}
-	dialer.SetMultipathTCP(true)
+	if *noMptcp {
+		log.Println("Creating single TCP connection...")
+	} else {
+		log.Println("Creating single MPTCP connection...")
+		dialer.SetMultipathTCP(true)
+	}
 	conn, err := dialer.Dial("tcp", remoteAddr)
 	if err != nil {
 		log.Fatal(err)
